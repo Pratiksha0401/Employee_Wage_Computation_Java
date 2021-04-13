5c57a9897e45b7b59718e4a8d18c4cb4be88e74b
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-04-13 16:51:45 +0530

    Added : code for part time employee wage

--- employee_wage_computation.java
+++ employee_wage_computation.java
@@ -3,16 +3,23 @@ class employee_wage_computation
 {
 	public static void main(String[] args) 
 	{
-		int is_Full_Time=1;
-		int empHr=8;
+		int is_Full_Time=2;
+		int is_Part_Time=1;
+		int fullTimeHr=8;
+		int partTimeHr=4;
 		int empWage_perHr=20;
 		int salary;
-		int empCheck=(int)Math.floor(Math.random()*10)%2;
+		int empCheck=(int)Math.floor(Math.random()*10)%3;
 
 		if(empCheck==is_Full_Time)
 		{
-			System.out.println("Employee is Present ");
-			salary=(empHr*empWage_perHr);
+			System.out.println("Employee is Present for Full Time ");
+			salary=(fullTimeHr*empWage_perHr);
+		}
+		else if(empCheck==is_Part_Time)
+		{
+			System.out.println("Employee is Present for Part Time");
+			salary=(partTimeHr*empWage_perHr);
 		}
 		else
 		{
